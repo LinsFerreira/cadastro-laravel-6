5b22ab0f4ae9784ff7385578f933bf1b33fbfdf7
Walterlins Ferreira <jpwdsf1968@gmail.com>
2021-01-26 21:26:22 -0300

    Primeiro Commit

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,36 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/nome-url', function() {
+    return 'Hey hey hey';
+});
+
+Route::get('/produtos/{idProduto?}', function($idProduto = '') {
+    return "Produto(s) {$idProduto}";
+});
+Route::get('/categoria/{flag}/posts', function($prm1) {
+    return "Posts da categoria: {$prm1}";
+});
+Route::get('/categorias/{flag}', function($prm1) {
+    return "Produtos da categoria: {$prm1}";
+});
+
+Route::match(['get','post'], '/match', function() {
+    return "Match";
+});
+Route::any('/any', function() {
+    return "Any";
+});
+
+Route::post('/register', function () {
+    return '';
+});
+Route::get('/empresa', function () {
+    return view('site.contact');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
